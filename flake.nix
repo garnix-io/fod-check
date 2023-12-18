e12a9751d9d911ae7bfc6e1437c634ce0d737a00
Alex David <git@alexd.me>
2023-12-18 10:18:01 -0800

    Remove trailing whitespaces

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: 
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import "${nixpkgs}" {
           inherit system;
@@ -26,7 +26,7 @@
           cp ${hashInput testCommand} $out
         '';
       in
-        { 
+        {
           lib = {
             inherit runTest version;
           };
