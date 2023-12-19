57d0da6de3ce04a02de285900052b27441bac45a
Sönke Hahn <soenkehahn@gmail.com>
2023-12-18 19:12:53 -0500

    wip

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
               waiter = pkgs.writeText "waiter" ''
                 fn main() {
                   for i in 0..1000 {
-                    println!("waiter: {i} 5");
+                    println!("waiter: {i} 6");
                     std::thread::sleep(std::time::Duration::from_secs(1));
                   }
                 }
