bd623fd26cd14becf7d32a72a8ce1d5c48ab93ea
Sönke Hahn <soenkehahn@gmail.com>
2023-12-18 19:26:37 -0500

    wip

--- flake.nix
+++ flake.nix
@@ -1,73 +1,31 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import "${nixpkgs}" {
           inherit system;
         };
-        escapeShellArg = pkgs.lib.strings.escapeShellArg;
-        exec = cmd: builtins.readFile (builtins.toString (pkgs.runCommand "exe" { } "${cmd} > $out"));
-        version = "2023-12-18";
-        hashInput = testCommand: pkgs.runCommand "gen-hash-input" { } ''
-          echo "${version}" > $out
-          echo ${escapeShellArg testCommand} | base64 >> $out
+        waiter = pkgs.writeText "waiter" ''
+          fn main() {
+            for i in 0..1000 {
+              println!("waiter: {i} 6");
+              std::thread::sleep(std::time::Duration::from_secs(1));
+            }
+          }
         '';
-        getHash = testCommand: exec "echo -n $(${pkgs.nix}/bin/nix-hash --type sha256 --base64 ${hashInput testCommand})";
-        runTest = buildInputs: testCommand: pkgs.runCommand "fod-test"
+        ifd = pkgs.runCommand "fod-in-ifd-test"
           {
-            buildInputs = [ pkgs.cacert ] ++ buildInputs;
-            outputHashMode = "recursive";
-            outputHashAlgo = "sha256";
-            outputHash = getHash testCommand;
+            nativeBuildInputs = [ pkgs.rustc pkgs.gcc ];
+            # outputHashMode = "recursive";
+            # outputHashAlgo = "sha256";
+            # outputHash = "sha256-iFSPu804N4Qh4x11uFnieiEUQKPmjGp3jArABb9E3Pc=";
           } ''
-          echo ${escapeShellArg "running test: \n${testCommand}"}
-          echo ---
-          ${testCommand}
-          cp ${hashInput testCommand} $out
+          rustc ${waiter} -o waiter2
+          ./waiter2
+          echo 'pkgs : { packages.default = pkgs.hello; }' > $out
         '';
       in
-      {
-        lib = {
-          inherit runTest version;
-        };
-
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [ pkgs.deno ];
-        };
-
-        packages = {
-          # networkInfo = runTest [ pkgs.nettools pkgs.iproute ] ''
-          #   ip --brief address show
-          #   exit 1
-          # '';
-          ifdTest =
-            let
-              waiter = pkgs.writeText "waiter" ''
-                fn main() {
-                  for i in 0..1000 {
-                    println!("waiter: {i} 6");
-                    std::thread::sleep(std::time::Duration::from_secs(1));
-                  }
-                }
-              '';
-              ifd = pkgs.runCommand "fod-in-ifd-test"
-                {
-                  buildInputs = [ pkgs.rustc pkgs.gcc ];
-                  # outputHashMode = "recursive";
-                  # outputHashAlgo = "sha256";
-                  # outputHash = "sha256-iFSPu804N4Qh4x11uFnieiEUQKPmjGp3jArABb9E3Pc=";
-                } ''
-                rustc ${waiter} -o waiter
-                ./waiter
-                echo '{}' > $out
-              '';
-            in
-            pkgs.runCommand "ifdTest" (import ifd) ''
-              echo ${builtins.toJSON (import ifd)}
-              echo ifdTest > $out
-            '';
-        };
-      }
+      (import ifd) pkgs
     );
 }
