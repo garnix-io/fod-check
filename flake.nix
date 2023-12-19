87b26a4b7cee53c9f1e89bc71b32c135db379c10
r-vdp <ramses@well-founded.dev>
2023-12-19 19:18:31 +0100

    Format with nixpkgs-fmt.

--- flake.nix
+++ flake.nix
@@ -1,48 +1,54 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils }:
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import "${nixpkgs}" {
-          inherit system;
-        };
-        escapeShellArg = pkgs.lib.strings.escapeShellArg;
-        exec = cmd: builtins.readFile (builtins.toString (pkgs.runCommand "exe" {} "${cmd} > $out"));
-        version = "2023-12-18";
-        hashInput = testCommand: pkgs.runCommand "gen-hash-input" {} ''
-          echo "${version}" > $out
-          echo ${escapeShellArg testCommand} | base64 >> $out
-        '';
-        getHash = testCommand: exec "echo -n $(${pkgs.nix}/bin/nix-hash --type sha256 --base64 ${hashInput testCommand})";
-        runTest = buildInputs: testCommand: pkgs.runCommand "fod-test" {
+    let
+      pkgs = import "${nixpkgs}" {
+        inherit system;
+      };
+      escapeShellArg = pkgs.lib.strings.escapeShellArg;
+      exec = cmd: builtins.readFile (builtins.toString (pkgs.runCommand "exe" { } "${cmd} > $out"));
+      version = "2023-12-18";
+      hashInput = testCommand: pkgs.runCommand "gen-hash-input" { } ''
+        echo "${version}" > $out
+        echo ${escapeShellArg testCommand} | base64 >> $out
+      '';
+      getHash = testCommand: exec "echo -n $(${pkgs.nix}/bin/nix-hash --type sha256 --base64 ${hashInput testCommand})";
+      runTest = buildInputs: testCommand: pkgs.runCommand "fod-test"
+        {
           buildInputs = [ pkgs.cacert ] ++ buildInputs;
           outputHashMode = "recursive";
           outputHashAlgo = "sha256";
           outputHash = getHash testCommand;
-        } ''
+        }
+        ''
           echo ${escapeShellArg "running test: \n${testCommand}"}
           echo ---
           ${testCommand}
           cp ${hashInput testCommand} $out
         '';
-      in
-        {
-          lib = {
-            inherit runTest version;
-          };
+    in
+    {
+      lib = {
+        inherit runTest version;
+      };
 
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [ pkgs.deno ];
-          };
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.deno ];
+      };
 
-          checks = {
-            passTest = runTest [] "true";
-            deno-tests = runTest [pkgs.deno pkgs.nix] ''
-              export HOME=$(pwd)
-              cd ${./.}
-              deno test --allow-read --allow-write --allow-run -- ${system}
-            '';
-          };
-        }
-    );
+      checks = {
+        passTest = runTest [ ] "true";
+        deno-tests = runTest [ pkgs.deno pkgs.nix ] ''
+          export HOME=$(pwd)
+          cd ${./.}
+          deno test --allow-read --allow-write --allow-run -- ${system}
+        '';
+      };
+    });
 }
